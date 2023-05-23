423a155eb71576d258f8fccf64539e4fb7423be1
sir-oro <marchiorofabio@protonmail.com>
2023-05-23 15:05:34 +0200

    remove sudo from check_services_status.sh to be able to schedule it through crontab

--- scripts/monitoring/check_services_status.sh
+++ scripts/monitoring/check_services_status.sh
@@ -5,56 +5,56 @@ path=$(echo $0 | sed 's|/.[^/]*.sh$||;s|\./||')
 . $path/send_message.sh
 
 # check bitcoind status if service is enabled
-bitcoind_enabled=$(sudo systemctl is-enabled bitcoind.service)
-bitcoind_status=$(sudo systemctl status bitcoind.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
+bitcoind_enabled=$(systemctl is-enabled bitcoind.service)
+bitcoind_status=$(systemctl status bitcoind.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
 if [ $bitcoind_enabled == "enabled" ] && [ $bitcoind_status != "running" ]
 then
   send "$(hostname): Bitcoin Core is $bitcoind_status"
 fi
 
 # check electrs status if service is enabled
-electrs_enabled=$(sudo systemctl is-enabled electrs.service)
-electrs_status=$(sudo systemctl status electrs.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
+electrs_enabled=$(systemctl is-enabled electrs.service)
+electrs_status=$(systemctl status electrs.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
 if [ $electrs_enabled == "enabled" ] && [ $electrs_status != "running" ]
 then
   send "$(hostname): Electrum Server is $electrum_status"
 fi
 
 # check btcrpcexplorer status if service is enabled
-btcrpcexplorer_enabled=$(sudo systemctl is-enabled btcrpcexplorer.service)
-btcrpcexplorer_status=$(sudo systemctl status btcrpcexplorer.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
+btcrpcexplorer_enabled=$(systemctl is-enabled btcrpcexplorer.service)
+btcrpcexplorer_status=$(systemctl status btcrpcexplorer.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
 if [ $btcrpcexplorer_enabled == "enabled" ] && [ $btcrpcexplorer_status != "running" ]
 then
   send "$(hostname): BTC RPC Explorer is $btcrpcexplorer_status"
 fi
 
 # check mempool status if service is enabled
-mempool_enabled=$(sudo systemctl is-enabled mempool.service)
-mempool_status=$(sudo systemctl status mempool.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
+mempool_enabled=$(systemctl is-enabled mempool.service)
+mempool_status=$(systemctl status mempool.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
 if [ $mempool_enabled == "enabled" ] && [ $mempool_status != "running" ]
 then
   send "$(hostname): Mempool is $mempool_status"
 fi
 
 # check jmwalletd status if service is enabled
-jmwalletd_enabled=$(sudo systemctl is-enabled jmwalletd.service)
-jmwalletd_status=$(sudo systemctl status jmwalletd.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
+jmwalletd_enabled=$(systemctl is-enabled jmwalletd.service)
+jmwalletd_status=$(systemctl status jmwalletd.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
 if [ $jmwalletd_enabled == "enabled" ] && [ $jmwalletd_status != "running" ]
 then
   send "$(hostname): JoinMarket is $jmwalletd_status"
 fi
 
 # check obwatcher status if service is enabled
-obwatcher_enabled=$(sudo systemctl is-enabled obwatcher.service)
-obwatcher_status=$(sudo systemctl status obwatcher.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
+obwatcher_enabled=$(systemctl is-enabled obwatcher.service)
+obwatcher_status=$(systemctl status obwatcher.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
 if [ $obwatcher_enabled == "enabled" ] && [ $obwatcher_status != "running" ]
 then
   send "$(hostname): Joinmarket - Orderbook Watcher is $obwatcher_status"
 fi
 
 # check jam status if service is enabled
-jam_enabled=$(sudo systemctl is-enabled jam.service)
-jam_status=$(sudo systemctl status jam.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
+jam_enabled=$(systemctl is-enabled jam.service)
+jam_status=$(systemctl status jam.service | grep Active | sed 's|.*Active:.*(||;s|).*||')
 if [ $jam_enabled == "enabled" ] && [ $jam_status != "running" ]
 then
   send "$(hostname): Jam is $jam_status"
