35dfecae38c26a6e0a7a77772a7d0c8eab64fca3
ciori <ciori@posteo.net>
2024-03-09 16:28:51 +0100

    Fix tor source list arch replacement

--- init.sh
+++ init.sh
@@ -49,7 +49,7 @@ sudo systemctl enable --now fail2ban
 
 # Add repos and keys
 sudo cp ${REPO_PATH}/templates/tor/tor.list /etc/apt/sources.list.d/tor.list
-sudo sed -i "s/SYS_DPKG_ARCH/${SYS_DPKG_ARCH}/g" /etc/apt/sources.list.d/tor.list
+sudo sed -i "s/SYS_ARCH/${SYS_DPKG_ARCH}/g" /etc/apt/sources.list.d/tor.list
 sudo sed -i "s/SYS_VERSION/${SYS_VERSION}/g" /etc/apt/sources.list.d/tor.list
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee /usr/share/keyrings/tor-archive-keyring.gpg >/dev/null
 
