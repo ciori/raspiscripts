6ad54362dc809defc8d8fec0864e32aa3496fe60
ciori <ciori@posteo.net>
2024-03-27 23:16:29 +0100

    Update scripts

--- change_ip.sh
+++ change_ip.sh
@@ -11,21 +11,23 @@ DIALOG_WIDTH=60
 
 # Show current IP address and ask to change it
 IP_ADDRESS=$(hostname -I)
-CHANGE_FLAG=$(dialog \
+dialog \
     --clear \
     --title "Change IP Address" \
     --yesno "Your current IP address is ${IP_ADDRESS}, would you like to change it?" \
     $DIALOG_HEIGHT $DIALOG_WIDTH \
-    2>&1 >/dev/tty)
+    2>&1 >/dev/tty
 
 # Ask if the user wants a DHCP or static IP
-if [ $CHANGE_FLAG ]
+if [ $? -eq 0 ]
 then
+    ITEMS=(1 "Automatic (DHCP)"
+           2 "Manual (Static)")
     IP_TYPE=$(dialog \
         --clear \
         --title "Change IP Address" \
         --menu "What type of IP address do you want??" \
-        $DIALOG_HEIGHT $DIALOG_WIDTH "Automatic (DHCP)" "Manual (Static)" \
+        $DIALOG_HEIGHT $DIALOG_WIDTH "${ITEMS[@]}" \
         2>&1 >/dev/tty)
     echo $IP_TYPE
 fi
