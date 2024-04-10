0dc2d256ca48fca8b32441794cd93dfee67a23ea
ciori <ciori@posteo.net>
2024-04-10 21:16:09 +0200

    Add check to after sync script

--- after_sync.sh
+++ after_sync.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-# Reduce the size of the database cache after sync
-sudo -u bitcoin sed -i "s/dbcache=2000/#dbcache=2000/g" /home/bitcoin/.bitcoin/bitcoin.conf
-sudo -u bitcoin sed -i "s/blocksonly=1/#blocksonly=1/g" /home/bitcoin/.bitcoin/bitcoin.conf
-sudo systemctl restart bitcoind
-echo "Size of the Bitcoin database cache reduced since syncing is finished"
+# Check whether the blockchain has finished syncing
+echo "Checking whether the blockchain has finished syncing..."
+BLOCKCHAIN_STATUS=$(bitcoin-cli getblockchaininfo)
+if [[ $BLOCKCHAIN_STATUS == *"\"initialblockdownload\": false"* ]]; then
+    # Reduce the size of the database cache after sync
+    sudo -u bitcoin sed -i "s/dbcache=2000/#dbcache=2000/g" /home/bitcoin/.bitcoin/bitcoin.conf
+    sudo -u bitcoin sed -i "s/blocksonly=1/#blocksonly=1/g" /home/bitcoin/.bitcoin/bitcoin.conf
+    sudo systemctl restart bitcoind
+    echo ""
+    echo "OK, size of the Bitcoin database cache has been reduced"
+    echo "You should now install other services!!!"
+    echo ""
+elif
+    echo ""
+    echo "ABORT, the blockchain is still synchronizing!!!"
+    echo ""
+fi
