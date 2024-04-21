46ac1159d6c896fd05b356af02be0d46ff7cf0c1
ciori <ciori@posteo.net>
2024-04-21 20:19:21 +0200

    Add mempool backend config

--- services/mempool/mempool_install.sh
+++ services/mempool/mempool_install.sh
@@ -17,10 +17,7 @@ SCRIPT_PATH=$(dirname "$SCRIPT")
 export $(xargs < ${SCRIPT_PATH}/../../envs)
 
 # Install nodejs with nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install 16
+sudo -u mempool bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash; export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; nvm install 16'
 
 # Allow mempool on firewall
 sudo firewall-cmd --permanent --zone=public --add-port=4081/tcp
@@ -41,7 +38,26 @@ MEMPOOL_VERSION=$(dialog \
     2>&1 >/dev/tty)
 sudo -u mempool bash -c "cd; cd mempool; git checkout $MEMPOOL_VERSION"
 
-# Setup mempool database
+# Setup mariadb
 sudo apt install -y mariadb-server mariadb-client
-#MARIADB_PASSWORD=$(gpg --gen-random --armor 1 16)
+MARIADB_PASSWORD=$(gpg --gen-random --armor 1 16)
+sudo mysql <<EOF
+create database mempool;
+grant all privileges on mempool.* to 'mempool'@'localhost' identified by '${MARIADB_PASSWORD}';
+EOF
+
+# Build backend
+sudo -u mempool bash -c 'cd /home/mempool/mempool/backend; export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; npm install --prod; npm run build'
+
+# Configure the mempool backend
+sudo cp ${SCRIPT_PATH}/../../templates/mempool/mempool-config.json /home/mempool/mempool/backend/mempool-config.json
+sudo -u mempool sed -i "s/Password_M/${MARIADB_PASSWORD}/g" /home/mempool/mempool/backend/mempool-config.json
+RPCAUTH_PASSWORD=$(dialog \
+    --clear \
+    --title "Setup Mempool Backend config credentials" \
+    --passwordbox "Insert the Bitcoin RPC Auth password initially generated with the init.sh script:" \
+    $DIALOG_HEIGHT $DIALOG_WIDTH \
+    2>&1 >/dev/tty)
+sudo -u mempool sed -i "s/Password_B/${RPCAUTH_PASSWORD}/g" /home/mempool/mempool/backend/mempool-config.json
+
 
