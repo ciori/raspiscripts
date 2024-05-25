460124b14e19e7dc8f8a9a7bfb4815d1e6a76aad
ciori <ciori@posteo.net>
2024-05-25 15:07:41 +0200

    Update jam resources

--- services/joinmarket/joinmarket_install.sh
+++ services/joinmarket/joinmarket_install.sh
@@ -93,8 +93,8 @@ sudo adduser --disabled-password --gecos "" jam
 sudo -u jam bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash; export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; nvm install 16'
 
 # Configure joinmarket for jam
-sudo sed -i "/max_cj_fee_rel/c\max_cj_fee_rel = 0.00003"  ${DATA_PATH}/joinmarket/joinmarket.cfg
-sudo sed -i "/max_cj_fee_abs/c\max_cj_fee_abs = 600"  ${DATA_PATH}/joinmarket/joinmarket.cfg
+sudo sed -i "/max_cj_fee_rel/c\max_cj_fee_rel = 0.00003" ${DATA_PATH}/joinmarket/joinmarket.cfg
+sudo sed -i "/max_cj_fee_abs/c\max_cj_fee_abs = 600" ${DATA_PATH}/joinmarket/joinmarket.cfg
 sudo -u joinmarket bash -c 'cd /home/joinmarket/.joinmarket; mkdir ssl/ && cd "$_"; openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out cert.pem -keyout key.pem -subj "/CN=localhost"'
 
 # Configure nginx
@@ -105,8 +105,44 @@ sudo systemctl reload nginx
 sudo firewall-cmd --permanent --zone=public --add-port=4020/tcp
 sudo firewall-cmd --reload
 
-# ...
+# Get the jam source code and ask for the version to use
+sudo -u jam bash -c 'curl https://dergigi.com/PGP.txt | gpg --import'
+sudo -u jam bash -c "cd; git clone https://github.com/joinmarket-webui/jam.git"
+JAM_VERSION_LATEST=$(curl "https://api.github.com/repos/joinmarket-webui/jam/releases/latest" -s | jq .name -r | awk '{print $1;}')
+JAM_VERSION=$(dialog \
+    --clear \
+    --title "Download Jam" \
+    --inputbox "What version would you like to download?" \
+    $DIALOG_HEIGHT $DIALOG_WIDTH $JAM_VERSION_LATEST \
+    2>&1 >/dev/tty)
+sudo -u jam bash -c "cd; cd jam; git checkout tags/${JAM_VERSION}; git verify-tag ${JAM_VERSION}"
 
+# Install jam
+sudo -u jam bash -c 'cd; cd jam; export NVM_DIR="$HOME/.nvm"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; npm install'
+sudo -u jam bash -c "cd; cd jam; touch .env"
+sudo -u jam bash -c 'grep -qxF "PORT=3020" /home/jam/jam/.env || echo "PORT=3020" >> /home/jam/jam/.env'
+
+# Setup systemd services
+sudo cp ${SCRIPT_PATH}/../../templates/joinmarket/jmwalletd.service /etc/systemd/system/jmwalletd.service
+sudo cp ${SCRIPT_PATH}/../../templates/joinmarket/obwatcher.service /etc/systemd/system/obwatcher.service
+sudo cp ${SCRIPT_PATH}/../../templates/joinmarket/jam.service /etc/systemd/system/jam.service
+sudo cp ${SCRIPT_PATH}/../../templates/joinmarket/jam-start.sh /home/jam/jam/jam-start.sh
+sudo chown jam:jam /home/jam/jam/jam-start.sh
+sudo chmod 700 /home/jam/jam/jam-start.sh
+sudo systemctl daemon-reload
+sudo systemctl enable --now jmwalletd
+sudo systemctl enable --now obwatcher
+sudo systemctl enable --now jam
+
+# Enable the tor hidden service
+if ! grep -q "hidden_service_jam" /etc/tor/torrc; then
+    echo "" | sudo tee -a /etc/tor/torrc
+    echo "HiddenServiceDir /var/lib/tor/hidden_service_jam/" | sudo tee -a /etc/tor/torrc
+    echo "HiddenServiceVersion 3" | sudo tee -a /etc/tor/torrc
+    echo "HiddenServicePort 443 127.0.0.1:4020" | sudo tee -a /etc/tor/torrc
+    sudo systemctl reload tor
+fi
+JAM_TOR=$(sudo cat /var/lib/tor/hidden_service_jam/hostname)
 
 # Final output
 echo ""
@@ -114,6 +150,5 @@ echo ""
 echo "Joinmarket and Jam has been installed!!!"
 echo ""
 echo "The Tor hidden service is: ${JAM_TOR}"
-echo "HTTP Port: xxxx"
-echo "HTTPS Port: xxxx"
+echo "HTTPS Port: 4020"
 echo ""
---
+++ templates/joinmarket/jam-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export NVM_DIR="/home/jam/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+npm start
